660d008657fd788bf8d7c80dd91af0fa4594da48
nechamiToledano <nechami3142@gmail.com>
2025-07-22 20:40:51 +0300

    add green scrren DTO

---
+++ Ffmpeg.API/DTOs/GreenScreenDto.cs
@@ -0,0 +1,11 @@
+﻿namespace FFmpeg.API.DTOs
+{
+    public class GreenScreenDto
+    {
+        public IFormFile VideoFile { get; set; }
+        public IFormFile BackgroundFile { get; set; }
+
+    }
+
+
+}

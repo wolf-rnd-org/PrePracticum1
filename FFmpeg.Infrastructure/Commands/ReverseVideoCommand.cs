31706659568fa314c22fc5f6c7e5f078e3549d36
Nava-151 <n87891@gmail.com>
2025-07-23 22:36:26 +0300

    add model and command for revers fearure

---
+++ FFmpeg.Infrastructure/Commands/ReverseVideoCommand.cs
@@ -0,0 +1,34 @@
+﻿using Ffmpeg.Command.Commands;
+using FFmpeg.Core.Models;
+using FFmpeg.Infrastructure.Services;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFmpeg.Infrastructure.Commands
+{
+    internal class ReverseVideoCommand : BaseCommand, ICommand<ReverseVideoModel>
+    {
+        private readonly ICommandBuilder _commandBuilder;
+
+        public ReverseVideoCommand(FFmpegExecutor executor, ICommandBuilder commandBuilder)
+            : base(executor)
+        {
+            _commandBuilder = commandBuilder ?? throw new ArgumentNullException(nameof(commandBuilder));
+        }
+
+        public async Task<CommandResult> ExecuteAsync(ReverseVideoModel model)
+        {
+            CommandBuilder = _commandBuilder
+                  .SetInput(model.InputFile)
+                  .AddOption($"-vf reverse")
+                  .SetInput(model.OutputFile);
+
+            return await RunAsync();
+        }
+
+    }
+}

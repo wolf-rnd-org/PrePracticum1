61b79cf396d75d183dabc0528d3ea3055aa56184
racheli3936 <racheli3936@gmail.com>
2025-07-24 23:30:07 +0300

    addPictureToModel

--- FFmpeg.Core/Models/PreviewModel.cs
+++ FFmpeg.Core/Models/PreviewModel.cs
@@ -9,6 +9,7 @@ namespace FFmpeg.Core.Models
    public class PreviewModel
     {
         public string InputFile { get; set; }
+        public string PictureFile { get; set; }
         public string OutputFile { get; set; }
         public string Timestamp { get; set; } = "00:00:05";
     }

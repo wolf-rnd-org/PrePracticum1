b02bf43189182860cf0987060d8442392e5eaea8
ziporaf <ziporaf@sqlink.com>
2025-07-27 15:36:44 +0300

    add black-and-white-filter command

--- FFmpeg.Infrastructure/Services/FFmpegServiceFactory.cs
+++ FFmpeg.Infrastructure/Services/FFmpegServiceFactory.cs
@@ -14,6 +14,8 @@ namespace FFmpeg.Infrastructure.Services
     public interface IFFmpegServiceFactory
     {
         ICommand<WatermarkModel> CreateWatermarkCommand();
+        ICommand<BlackAndWhiteModel> CreateBlackAndWhiteCommand();
+
     }
 
     public class FFmpegServiceFactory : IFFmpegServiceFactory
@@ -35,6 +37,10 @@ namespace FFmpeg.Infrastructure.Services
         public ICommand<WatermarkModel> CreateWatermarkCommand()
         {
             return new WatermarkCommand(_executor, _commandBuilder);
+        }
+        public ICommand<BlackAndWhiteModel> CreateBlackAndWhiteCommand()
+        {
+            return new BlackAndWhiteCommand(_executor, _commandBuilder);
         }
     }
 }

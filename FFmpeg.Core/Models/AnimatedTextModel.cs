e8e76449d3ebe34e9c119ad25d574757161fd97c
hadasa51610 <hadasa5161@gmail.com>
2025-07-27 17:19:26 +0300

    Add animated text

---
+++ FFmpeg.Core/Models/AnimatedTextModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFmpeg.Core.Models
+{
+    public class AnimatedTextModel
+    {
+        public string InputFile { get; set; }
+        public string OutputFile { get; set; }
+        public string Content { get; set; }
+        public int XPosition { get; set; }
+        public int YPosition { get; set; }
+        public int FontSize { get; set; }
+        public string FontColor { get; set; } = "black";
+        public string VideoCodec { get; set; } = "libx264"; 
+        public bool IsVideo { get; set; } = true;
+        public bool IsAnimated { get; set; } = false;
+        public int AnimationSpeed { get; set; } = 100;
+    }
+}

31b2156b1d6ee4cd91e7b198611d34f7b2a6cb84
hadasa51610 <hadasa5161@gmail.com>
2025-07-30 15:40:39 +0300

    change code

--- FFmpeg.Infrastructure/Services/FFmpegServiceFactory.cs
+++ FFmpeg.Infrastructure/Services/FFmpegServiceFactory.cs
@@ -33,78 +33,76 @@ namespace FFmpeg.Infrastructure.Services
 
         ICommand<PreviewModel> CreatePreviewCommand();
     }
-
-    public class FFmpegServiceFactory : IFFmpegServiceFactory
+}
+public class FFmpegServiceFactory : IFFmpegServiceFactory
+{
+    private readonly FFmpegExecutor _executor;
+    private readonly ICommandBuilder _commandBuilder;
+    public FFmpegServiceFactory(IConfiguration configuration, ILogger logger = null)
     {
-        private readonly FFmpegExecutor _executor;
-        private readonly ICommandBuilder _commandBuilder;
-        public FFmpegServiceFactory(IConfiguration configuration, ILogger logger = null)
-        {
-            string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
-            string ffmpegPath = Path.Combine(baseDirectory, "external", "ffmpeg.exe");
-            bool logOutput = bool.TryParse(configuration["FFmpeg:LogOutput"], out bool log) && log;
-            _executor = new FFmpegExecutor(ffmpegPath, logOutput, logger);
-            _commandBuilder = new CommandBuilder(configuration);
-        }
-        public ICommand<WatermarkModel> CreateWatermarkCommand()
-        {
-            return new WatermarkCommand(_executor, _commandBuilder);
-        }
+        string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+        string ffmpegPath = Path.Combine(baseDirectory, "external", "ffmpeg.exe");
+        bool logOutput = bool.TryParse(configuration["FFmpeg:LogOutput"], out bool log) && log;
+        _executor = new FFmpegExecutor(ffmpegPath, logOutput, logger);
+        _commandBuilder = new CommandBuilder(configuration);
+    }
+    public ICommand<WatermarkModel> CreateWatermarkCommand()
+    {
+        return new WatermarkCommand(_executor, _commandBuilder);
+    }
 
-        public ICommand<ExtractFrameInput> CreateExtractFrameCommand()
-        {
-            return new ExtractFrameCommand(_executor, _commandBuilder);
-        }
+    public ICommand<ExtractFrameInput> CreateExtractFrameCommand()
+    {
+        return new ExtractFrameCommand(_executor, _commandBuilder);
+    }
 
-        public ICommand<FadeEffectModel> CreateFadeEffectCommand()
-        {
-            return new FadeEffectCommand(_executor, _commandBuilder);
-        }
-        public ICommand<ReverseVideoModel> CreateReverseVideoCommand()
-        {
-            return new ReverseVideoCommand(_executor, _commandBuilder);
-        }
-        public ICommand<GreenScreenModel> CreateGreenScreenCommand()
-        {
-            return new GreenScreenCommand(_executor, _commandBuilder);
-        }
-        public ICommand<BorderModel> CreateBorderCommand()
-        {
-            return new BorderCommand(_executor, _commandBuilder);
-        }
-        public ICommand<AnimatedTextModel> CreateAnimatedTextCommand()
-        {
-            return new AnimatedTextCommand(_executor, _commandBuilder);
-        }
-        public ICommand<TimestampOverlayModel> CreateTimestampOverlayCommand()
-        {
-            return new TimestampOverlayCommand(_executor, _commandBuilder);
-        }
+    public ICommand<FadeEffectModel> CreateFadeEffectCommand()
+    {
+        return new FadeEffectCommand(_executor, _commandBuilder);
+    }
+    public ICommand<ReverseVideoModel> CreateReverseVideoCommand()
+    {
+        return new ReverseVideoCommand(_executor, _commandBuilder);
+    }
+    public ICommand<GreenScreenModel> CreateGreenScreenCommand()
+    {
+        return new GreenScreenCommand(_executor, _commandBuilder);
+    }
+    public ICommand<BorderModel> CreateBorderCommand()
+    {
+        return new BorderCommand(_executor, _commandBuilder);
+    }
+    public ICommand<AnimatedTextModel> CreateAnimatedTextCommand()
+    {
+        return new AnimatedTextCommand(_executor, _commandBuilder);
+    }
+    public ICommand<TimestampOverlayModel> CreateTimestampOverlayCommand()
+    {
+        return new TimestampOverlayCommand(_executor, _commandBuilder);
+    }
 
 
-        public ICommand<ChangeSpeedModel> CreateChangeSpeedCommand()
-        {
-            return new ChangeSpeedCommand(_executor, _commandBuilder);
-        }
-        public ICommand<AudioEffectModel> CreateAudioEffectCommand()
-        {
-            return new AudioEffectCommand(_executor, _commandBuilder);
-        }
-        public ICommand<ConvertAudioModel> CreateConvertAudioCommand()
-        {
-            return new ConvertAudioCommand(_executor, _commandBuilder);
+    public ICommand<ChangeSpeedModel> CreateChangeSpeedCommand()
+    {
+        return new ChangeSpeedCommand(_executor, _commandBuilder);
+    }
+    public ICommand<AudioEffectModel> CreateAudioEffectCommand()
+    {
+        return new AudioEffectCommand(_executor, _commandBuilder);
+    }
+    public ICommand<ConvertAudioModel> CreateConvertAudioCommand()
+    {
+        return new ConvertAudioCommand(_executor, _commandBuilder);
 
 
-        }
-        public ICommand<PreviewModel> CreatePreviewCommand()
-        {
-            return new PreviewCommand(_executor, _commandBuilder);
-        }
+    }
+    public ICommand<PreviewModel> CreatePreviewCommand()
+    {
+        return new PreviewCommand(_executor, _commandBuilder);
+    }
 
-        public ICommand<MergeTwoFilesModel> CreateMergeTwoFilesCommand()
-        {
-            return new MergeTwoFilesCommand(_executor, _commandBuilder);
-        }
+    public ICommand<MergeTwoFilesModel> CreateMergeTwoFilesCommand()
+    {
+        return new MergeTwoFilesCommand(_executor, _commandBuilder);
     }
 }
-
